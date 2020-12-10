a574cdce89976ad33127a74c414c15b202b20559
luis <luishenriquesimoes@yahoo.com.br>
2020-12-10 14:27:54 -0300

    doctor validations corrected and first seed done

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,7 +3,7 @@ class Doctor < ApplicationRecord
   validates :crm, presence: true,
                   uniqueness: true,
                   format: {
-                    with: /\A\d{2}-\d{5}\.\d\z/,
+                    with: /\A\d{2}\.\d{5}-\d\z/,
                     message: "Por favor insira um formato válido de CRM (xx-xxxxx.x)"
                   }
   validates :first_name, presence: true, uniqueness: { scope: :last_name, message: "Esse nome já existe" }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning doctors from DB..."
+Doctor.destroy_all
+puts "No more old doctors here!"
+
+doctors = [
+  {
+    first_name: "Laura",
+    last_name: "Meyer",
+    crm: "52.96891-9"
+  },
+  {
+    first_name: "Rafael",
+    last_name: "Ono Furukawa",
+    crm: "52.96988-0"
+  },
+  {
+    first_name: "Juliana",
+    last_name: "O. Matias",
+    crm: "52.89952-6"
+  },
+  {
+    first_name: "Simone",
+    last_name: "Maradei",
+    crm: "52.65510-4"
+  },
+  {
+    first_name: "Mayara",
+    last_name: "Rêgo",
+    crm: "52.98995-9"
+  }
+]
+
+puts "Creating new doctors DB:"
+
+doctors.each do |doctor|
+  Doctor.create(doctor)
+end
