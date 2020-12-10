46eb395dd706422ed2f3ca0f79dee07f0c9b92ab
Vinicius Stelet <vinicius.bootcamp@gmail.com>
2020-12-10 15:46:57 -0300

    Created routes and views for procedures and procedures/new

---
+++ app/controllers/procedures_controller.rb
@@ -0,0 +1,26 @@
+class ProceduresController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :set_procedure
+
+  def index
+    @procedures = Procedure.all
+  end
+
+  def new
+    @procedure = Procedure.new
+  end
+
+  def create
+    @procedure = Procedure.create(params_procedure)
+  end
+
+  private
+
+  def params_procedure
+    params.require(:procedure).permit(:cd_exame, :cd_sus, :description)
+  end
+
+  def set_procedure
+    @procedure = Procedure.new
+  end
+end
--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,3 +1,5 @@
 class Procedure < ApplicationRecord
   has_many :exams
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :cd_exame, presence: true
 end
