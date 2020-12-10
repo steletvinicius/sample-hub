40c113465f6987564053c2357cc5c82e54c03e12
Vinicius Stelet <vinicius.bootcamp@gmail.com>
2020-12-10 13:11:57 -0300

    SUS procedures seed implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Populating procedures table with standard sus procedures"
+
+cd_exames = [
+  "03_DMCF1",
+  "04_DMCF2",
+  "05_DMCF3",
+  "01_RMCF1",
+  "02_RMCF2",
+  "06_RMRF1",
+  "07_RMRF2"
+]
+cd_sus = [
+  "0501010025",
+  "0501010033",
+  "0501010041",
+  "0501020012",
+  "0501020020",
+  "0501020012",
+  "0501020020"
+]
+
+descriptions = [
+  "DOA APARENT MO CEMO FASE 1. HLA A, B (SOROL OU BIOMOL BAIXA RESOL)",
+  "DOA APARENT MO CEMO FASE 2. HLA DRB1, DQB1 (BIOMOL BAIXA RESOL)",
+  "DOA APARENT MO CEMO FASE 3. HLA DRB1, DQB1 (BIOMOL ALTA RESOL)",
+  "REC APARENT MO CEMO FASE 1. HLA A, B(SOROL OU BIOMOL BAIXA RESOL) HLA DRB1, DQB1(BIOMOL BAIXA RESOL)",
+  "REC APARENT MO CEMO FASE 2. HLA DRB1, DQB1 (BIOMOL ALTA RESOL)",
+  "REC NAO APARENT MO REREME FASE 1. HLA A, B (SOROL OU BIOMOL BAIXA RESOL) HLA DRB1, DQB1 (BIOMOL BAIXA RESOL)",
+  "REC NAO APARENT MO REREME FASE 2. HLA DRB1, DQB1 (BIOMOL ALTA RESOL)"
+]
+x = 0
+begin
+  Procedure.create!(
+    cd_exame: cd_exames[x],
+    cd_sus: cd_sus[x],
+    description: descriptions[x]
+    )
+  puts "Procedure #{cd_exames[x]} created."
+  x += 1
+end while x < cd_exames.length
+
+puts "Created #{Procedure.count} SUS procedures."
+
