3bd4438cf99973c0b12905dc433f8dca3fc0f4e4
Edson <edson.dario@gmail.com>
2020-12-10 13:50:41 -0300

    seed patientes

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,4 +1,11 @@
 class Family < ApplicationRecord
   belongs_to :receptor, foreign_key: 'receptor_id', class_name: 'Patient'
   belongs_to :donor, foreign_key: 'donor_id', class_name: 'Patient'
+
+  validates :receptor_id, presence: true
+  validates :donor_id, presence: true
+  validates :relationship, presence: true, inclusion: { in: [
+    "mãe", "pai", "irmão(ã)", "conjugue", "companheir(a)o", "avó(ô)",
+    "prima(o)", "tia(o)"
+  ] }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+##### BEGIN - SEED - Patients #####
+puts "Make patients... 'May the Force be with you.'"
+Patient.create(
+  first_name: "Luke",
+  last_name: "Skywalker",
+  gender: "M",
+  born_at: "01/03/1970",
+  address: "Brasília, Distrito Federal, Brasil",
+  cpf: "26741219048",
+  sus_code: "158568593440006"
+)
+
+Patient.create(
+  first_name: "Leia",
+  last_name: "Organa",
+  gender: "F",
+  born_at: "15/07/1980",
+  address: "SCN Quadra 2, Brasília, Distrito Federal, Brasil",
+  cpf: "09141415035",
+  sus_code: "958532819530006"
+)
+
+Patient.create(
+  first_name: "Obi-Wan",
+  last_name: "Kenobi",
+  gender: "M",
+  born_at: "03/04/1965",
+  address: "Parque da Cidade Dona Sarah Kubitschek, Brasília, Distrito Federal, Brasil",
+  cpf: "85154396030",
+  sus_code: "214233320680006"
+)
+puts "Patients ready. 'Your eyes can deceive you, don't trust them.'"
+##### END - SEED - Patients #####
