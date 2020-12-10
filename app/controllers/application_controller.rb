6b29cef669345a072d734d30a044b8c744f44db9
Edson <edson.dario@gmail.com>
2020-12-10 16:24:57 -0300

    fix_authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   skip_before_action :authenticate_user!, only: :home
 
