af96f9b0719787ba4318eb97693188636b578de5
Claudia Tanaka <catanaka@hotmail.com>
2020-12-10 08:12:44 -0300

    validações de User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,12 @@ class User < ApplicationRecord
   has_many :batch_receiver, foreign_key: 'user_lab_id', class_name: 'Batch'
   has_many :batch_sender, foreign_key: 'user_hospital_id', class_name: 'Batch'
 
+  validates :first_name, :last_name, presence: true, format: { with: /\D+/ } # accepts any non-digit char
+  validates :role, inclusion: { in: [
+                                    'Envio de amostras',
+                                    'Recepção de amostras',
+                                    'Administrador'
+                                    ], allow_nil: false }
   validates :registration_id, uniqueness: true
-  validates :role, inclusion: { in: ['Envio de amostras','Recepção de amostras','Administrador'], allow_nil: false }
-  validates :first_name, :last_name, presence: true, 
   # Se role = Envio ou Recepção de amostra, exigir preenchimento de institution e matrícula ?
 end
