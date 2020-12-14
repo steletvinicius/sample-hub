06e6c26c7aae9196b9898ac3af5a2b3089ae8b6c
Edson <edson.dario@gmail.com>
2020-12-14 12:48:18 -0300

    add_select2_to_patients_index

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,7 +2,7 @@ class PatientsController < ApplicationController
   before_action :set_patient, only: %i[show]
 
   def index
-    @query = policy_scope(Patient).order(created_at: :desc)
+    @patients = policy_scope(Patient).order(created_at: :desc)
   end
 
   def show
