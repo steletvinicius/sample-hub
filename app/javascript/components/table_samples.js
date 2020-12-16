55c7f3a11e8ca08139475ef7af679d04e42efcc2
luis <luishenriquesimoes@yahoo.com.br>
2020-12-16 17:19:51 -0300

    guard clauses were created at JS plugins and components

--- app/javascript/components/table_samples.js
+++ app/javascript/components/table_samples.js
@@ -7,92 +7,97 @@ const pluralizeJS = (count, singular, plural) =>
 const initFilterTblSamples = () => {
     // Loop through all rows, and hide those that don't match criteria
     const table = document.getElementById("tblSamples");
-    const tr = table.getElementsByTagName("tr");
-    let counter = 0;
+    if (table) {
 
-    for (let i = 0; i < tr.length; i++) {
-      const dateCell = tr[i].getElementsByTagName("td")[3]; // <==== ALTERAR COLUNA AQUI
-
-      if (dateCell) {
-        const dateValue = dateCell.textContent || dateCell.innerText;
-        console.log(dateValue);
-        if (dateValue !== "") {
-          tr[i].style.display = "";
-          counter += 1;
-        } else {
-          tr[i].style.display = "none";
+      const tr = table.getElementsByTagName("tr");
+      let counter = 0;
+      
+      for (let i = 0; i < tr.length; i++) {
+        const dateCell = tr[i].getElementsByTagName("td")[3]; // <==== ALTERAR COLUNA AQUI
+        
+        if (dateCell) {
+          const dateValue = dateCell.textContent || dateCell.innerText;
+          console.log(dateValue);
+          if (dateValue !== "") {
+            tr[i].style.display = "";
+            counter += 1;
+          } else {
+            tr[i].style.display = "none";
+          }
         }
       }
+      const caption = document.getElementById("counter");
+      caption.innerHTML = `Total ${pluralizeJS(counter, 'remessa', 'remessas')}`;
     }
-    const caption = document.getElementById("counter");
-    caption.innerHTML = `Total ${pluralizeJS(counter, 'remessa', 'remessas')}`;
-
 }
 
 const filterTblSamplesCollected = () => {
   const tabCollected = document.getElementById('filterCollected');
   const tabEmpty = document.getElementById('filterEmpty');
-
-  tabEmpty.addEventListener('click', () => {
-    // add selected class to tab element
-    tabCollected.classList.remove("selected");
-    tabCollected.classList.remove("btn-secondary");
-    tabEmpty.classList.add("selected");
-    tabEmpty.classList.add("btn-secondary");
-    // Loop through all rows, and hide those that don't match criteria
-    const table = document.getElementById("tblSamples");
-    const tr = table.getElementsByTagName("tr");
-    let counter = 0;
-
-    for (let i = 0; i < tr.length; i++) {
-      const dateCell = tr[i].getElementsByTagName("td")[3]; // <==== ALTERAR COLUNA AQUI
-
-      if (dateCell) {
-        const dateValue = dateCell.textContent || dateCell.innerText;
-        if (dateValue !== "") {
-          tr[i].style.display = "";
-          counter += 1;
-        } else {
-          tr[i].style.display = "none";
+  if (tabEmpty) {
+    tabEmpty.addEventListener('click', () => {
+      // add selected class to tab element
+      tabCollected.classList.remove("selected");
+      tabCollected.classList.remove("btn-secondary");
+      tabEmpty.classList.add("selected");
+      tabEmpty.classList.add("btn-secondary");
+      // Loop through all rows, and hide those that don't match criteria
+      const table = document.getElementById("tblSamples");
+      const tr = table.getElementsByTagName("tr");
+      let counter = 0;
+      
+      for (let i = 0; i < tr.length; i++) {
+        const dateCell = tr[i].getElementsByTagName("td")[3]; // <==== ALTERAR COLUNA AQUI
+        
+        if (dateCell) {
+          const dateValue = dateCell.textContent || dateCell.innerText;
+          if (dateValue !== "") {
+            tr[i].style.display = "";
+            counter += 1;
+          } else {
+            tr[i].style.display = "none";
+          }
         }
       }
-    }
-    const caption = document.getElementById("counter");
-    caption.innerHTML = `Total ${pluralizeJS(counter, 'remessa', 'remessas')}`;
-  });
+      const caption = document.getElementById("counter");
+      caption.innerHTML = `Total ${pluralizeJS(counter, 'remessa', 'remessas')}`;
+    });
+  }
 }
 
 const filterTblSamplesUncollected = () => {
   const tabCollected = document.getElementById('filterCollected');
   const tabEmpty = document.getElementById('filterEmpty');
+  if (tabEmpty) {
 
-  tabEmpty.addEventListener('click', () => {
-    // add selected class to tab element
-    tabCollected.classList.remove("selected");
-    tabCollected.classList.remove("btn-secondary");
-    tabEmpty.classList.add("selected");
-    tabEmpty.classList.add("btn-secondary");
-    // Loop through all rows, and hide those that don't match criteria
-    const table = document.getElementById("tblSamples");
-    const tr = table.getElementsByTagName("tr");
-    let counter = 0;
-
-    for (let i = 0; i < tr.length; i++) {
-      const dateCell = tr[i].getElementsByTagName("td")[3]; // <==== ALTERAR COLUNA AQUI
-
-      if (dateCell) {
-        const dateValue = dateCell.textContent || dateCell.innerText;
-        if (dateValue === "") {
-          tr[i].style.display = "";
-          counter += 1;
-        } else {
-          tr[i].style.display = "none";
+    tabEmpty.addEventListener('click', () => {
+      // add selected class to tab element
+      tabCollected.classList.remove("selected");
+      tabCollected.classList.remove("btn-secondary");
+      tabEmpty.classList.add("selected");
+      tabEmpty.classList.add("btn-secondary");
+      // Loop through all rows, and hide those that don't match criteria
+      const table = document.getElementById("tblSamples");
+      const tr = table.getElementsByTagName("tr");
+      let counter = 0;
+      
+      for (let i = 0; i < tr.length; i++) {
+        const dateCell = tr[i].getElementsByTagName("td")[3]; // <==== ALTERAR COLUNA AQUI
+        
+        if (dateCell) {
+          const dateValue = dateCell.textContent || dateCell.innerText;
+          if (dateValue === "") {
+            tr[i].style.display = "";
+            counter += 1;
+          } else {
+            tr[i].style.display = "none";
+          }
         }
       }
-    }
-    const caption = document.getElementById("counter");
-    caption.innerHTML = `Total ${pluralizeJS(counter, 'remessa', 'remessas')}`;
-  });
+      const caption = document.getElementById("counter");
+      caption.innerHTML = `Total ${pluralizeJS(counter, 'remessa', 'remessas')}`;
+    });
+  }
 }
 
 export { filterTblSamplesCollected };
