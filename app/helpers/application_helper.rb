18dbe9fb8520c3e3ff866ef8a29336ccae79bdcd
Vinicius Stelet <vinicius.bootcamp@gmail.com>
2020-12-16 15:48:58 -0300

    Implementado formulario de coleta na show de sample

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,10 @@ module ApplicationHelper
     date.strftime("%d/%m - %H:%M") unless date.nil?
   end
 
+  def dateyeartime_mask(date)
+    date.strftime("%d/%m/%Y - %H:%M") unless date.nil?
+  end
+
   # ONLY WORKS ON VIEWS - NOT FOR CONTROLLERS / PUNDIT POLICIES
   # checks if user role is sender or receiver to help customize views for each role
   def sender?(user)
@@ -23,5 +27,9 @@ module ApplicationHelper
   def full_title(page_title = '')
     base_title = 'Sample Hub'
     page_title.empty? ? base_title : "#{base_title} | #{page_title}"
+  end
+
+  def calc_age(date)
+    ((DateTime.now - date) / 365.25).to_i
   end
 end
