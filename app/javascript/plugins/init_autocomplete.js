34a455de22f7193ab27f977ebb2a95b6e7318b92
luis <luishenriquesimoes@yahoo.com.br>
2020-12-18 03:02:34 -0300

    orgulhosamente algoliados

---
+++ app/javascript/plugins/init_autocomplete.js
@@ -0,0 +1,10 @@
+import places from 'places.js';
+
+const initAutocomplete = () => {
+  const addressInput = document.getElementById('patient_address');
+  if (addressInput) {
+    places({ container: addressInput });
+  }
+};
+
+export { initAutocomplete };
