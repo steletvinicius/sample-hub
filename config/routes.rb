6c659926a3acf4fdc13524ee7def6b76e8512abc
Vinicius Stelet <vinicius.bootcamp@gmail.com>
2020-12-21 16:13:26 -0300

    Rails admin configured and running for admin:true users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
   get 'about_us', to: 'pages#about_us', as: :about_us
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_125949) do
+ActiveRecord::Schema.define(version: 2020_12_19_125828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
